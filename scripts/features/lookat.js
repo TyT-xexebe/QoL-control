25b296d0a2f08a6751bf130e0c9d533bbffbd0a9
TyT|xexebe <aspipaht007@gmail.com>
2026-03-19 22:26:41 +0200

    Changes:
    !lookat now shows markers on map (can be hidden by hold on them)
    
    	modified:   scripts/features/lookat.js

--- scripts/features/lookat.js
+++ scripts/features/lookat.js
@@ -1,20 +1,22 @@
 const notify = require("qol-control/core/logger").notify;
+const interceptor = require("qol-control/core/interceptor");
 
 let coordHistory = [];
 
 Events.on(PlayerChatEvent, e => {
-	if (!e.player) return; 
-	
+    if (!e.player) return; 
+    
     let raw = String(e.message);
     let match = raw.match(/(\d\d?\d?\d?)([ ,./|][ ,./|]?[ ,./|]?)(\d\d?\d?\d?)/);
 
     if (match) {
         let x = parseInt(match[1]), y = parseInt(match[3]);
-        if (x >= 0 && x <= 9999 && y >= 0 && y <= 9999) {
+        if (x >= 0 && x <= 1000 && y >= 0 && y <= 1000) {
             coordHistory.push({
                 nick: Strings.stripColors(e.player.name),
                 x: x,
-                y: y
+                y: y,
+                hidden: false
             });
             if (coordHistory.length > 9) coordHistory.shift();
         }
@@ -25,7 +27,89 @@ Events.on(WorldLoadEvent, () => {
     coordHistory = [];
 });
 
-const interceptor = require("qol-control/core/interceptor");
+let touchTimer = 0;
+let lastTouchX = 0;
+let lastTouchY = 0;
+let isTouching = false;
+
+Events.run(Trigger.update, () => {
+    if (Core.input.isTouched()) {
+        let wx = Core.input.mouseWorldX();
+        let wy = Core.input.mouseWorldY();
+        
+        if (!isTouching) {
+            isTouching = true;
+            touchTimer = 0;
+            lastTouchX = wx;
+            lastTouchY = wy;
+        } else {
+            if (Mathf.dst(wx, wy, lastTouchX, lastTouchY) > 8) {
+                touchTimer = 0;
+                lastTouchX = wx;
+                lastTouchY = wy;
+            } else {
+                touchTimer += Time.delta;
+                if (touchTimer > 30) {
+                    checkLongPress(wx, wy);
+                    touchTimer = -9999;
+                }
+            }
+        }
+    } else {
+        isTouching = false;
+        touchTimer = 0;
+    }
+});
+
+function checkLongPress(wx, wy) {
+    let scl = Core.camera.width / Core.graphics.getWidth();
+    let r = Math.max(40, 150 * scl);
+
+    for (let i = 0; i < coordHistory.length; i++) {
+        let c = coordHistory[i];
+        if (!c.hidden) {
+            if (Mathf.dst(wx, wy, c.x * 8, c.y * 8) < r) {
+                c.hidden = true;
+                break;
+            }
+        }
+    }
+}
+
+Events.run(Trigger.draw, () => {
+    let scl = Core.camera.width / Core.graphics.getWidth();
+    let r = Math.max(40, 150 * scl);
+    let pulse = Mathf.absin(Time.time, 8, r * 0.05);
+    let fScale = Math.max(0.25, 0.8 * scl);
+
+    for (let i = 0; i < coordHistory.length; i++) {
+        let c = coordHistory[i];
+        if (c.hidden) continue;
+
+        let wx = c.x * 8;
+        let wy = c.y * 8;
+
+        Draw.color(Pal.accent);
+        Draw.alpha(0.3 + Mathf.absin(Time.time, 8, 0.7));
+        Lines.stroke(2 + scl);
+        Lines.circle(wx, wy, r + pulse);
+        
+        Draw.reset();
+
+        let text = java.lang.String.valueOf(c.nick + " (" + c.x + ", " + c.y + ")");
+        let font = Fonts.outline;
+        
+        let oldScaleX = font.getData().scaleX;
+        let oldScaleY = font.getData().scaleY;
+        
+        try {
+            font.getData().setScale(fScale);
+            font.draw(text, wx, wy + r + pulse + (16 * fScale), 0, 1, false);
+        } finally {
+            font.getData().setScale(oldScaleX, oldScaleY);
+        }
+    }
+});
 
 const lookatHandler = (args) => {
     if (args[1] === "last" || args[1] === "l") {
@@ -43,7 +127,8 @@ const lookatHandler = (args) => {
                 let str = "";
                 for (let i = 0; i < coordHistory.length; i++) {
                     let c = coordHistory[i];
-                    str += "\n[lightgrey]" + (i + 1) + " - " + c.nick + "[lightgrey] - [accent]" + c.x + " " + c.y;
+                    let hiddenTag = c.hidden ? " [darkgrey](hidden)[]" : "";
+                    str += "\n[lightgrey]" + (i + 1) + " - " + c.nick + "[lightgrey] - [accent]" + c.x + " " + c.y + hiddenTag;
                 }
                 notify(str);
             }
