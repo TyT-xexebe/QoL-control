d07444a8fb0f9e8f414030bdd5c46c16fdf2ab88
TyT|xexebe <aspipaht007@gmail.com>
2026-05-17 17:56:34 +0300

    Changes:
    a little fix in multitask
    added plan range which shows blocks range while they in your build plan
    
    	modified:   mod.json
      modified:   scripts/features/multitask.js
    	new file:   scripts/features/plan_range.js
    	modified:   scripts/settings.json

--- scripts/features/multitask.js
+++ scripts/features/multitask.js
@@ -1,117 +1,89 @@
 const logger = require('qol-control/core/logger');
-let enabled = false;
-let moduleName = 'multitask';
 let lastUnitId = -1;
 let lastRotation = 0;
 
-function init() {
-	enabled = Core.settings.getBool('qol-control-multitask', true);
-	if (!enabled) return;
-
-	logger.info('Loaded multitask module.');
-
-	Events.run(Trigger.preDraw, () => {
-		let p = Vars.player;
-		if (!p || !p.unit() || !p.unit().type || !p.unit().type.hasWeapons) {
-			lastUnitId = -1;
-			return;
-		}
-
-		let isBuilding = p.unit().activelyBuilding();
-		let isMining = p.unit().mining();
-
-		if (!isBuilding && !isMining) {
-			lastUnitId = -1;
-			return;
-		}
-
-		let shouldShoot = false;
-		let boosted = false;
-
-		if (!Vars.mobile) {
-			shouldShoot = Core.input.keyDown(Binding.select);
+Events.run(Trigger.preDraw, () => {
+	let p = Vars.player;
+	if (!p || !p.unit() || !p.unit().type || !p.unit().type.hasWeapons) {
+		lastUnitId = -1;
+		return;
+	}
+	let isBuilding = p.unit().activelyBuilding();
+	let isMining = p.unit().mining();
+	if (!isBuilding && !isMining) {
+		lastUnitId = -1;
+		return;
+	}
+	let shouldShoot = false;
+	let boosted = false;
+	if (!Vars.mobile) {
+		shouldShoot = Core.input.keyDown(Binding.select);
+		boosted = p.unit().isFlying() && p.unit().type.canBoost;
+	} else {
+		let state = Vars.state;
+		if (!state.isEditor()) {
 			boosted = p.unit().isFlying() && p.unit().type.canBoost;
-		} else {
-			let state = Vars.state;
-			if (!state.isEditor()) {
-				boosted = p.unit().isFlying() && p.unit().type.canBoost;
-
-				let type = p.unit().type;
-				let enemy = Units.closestTarget(
-					p.unit().team,
-					p.unit().x,
-					p.unit().y,
-					type.maxRange,
-					(u) => true,
-					(t) => true
-				);
-
-				if (enemy || Core.input.isTouched()) {
-					shouldShoot = true;
-				}
-			}
-		}
-
-		if (shouldShoot && !boosted) {
-			let aimX = 0;
-			let aimY = 0;
 			let type = p.unit().type;
-
-			if (!Vars.mobile) {
-				let mouse = Core.input.mouseWorld();
-				aimX = mouse.x;
-				aimY = mouse.y;
-			} else {
-				let enemy = Units.closestTarget(
-					p.unit().team,
-					p.unit().x,
-					p.unit().y,
-					type.maxRange,
-					(u) => true,
-					(t) => true
-				);
-				if (enemy) {
-					aimX = enemy.x;
-					aimY = enemy.y;
-				} else {
-					aimX = Core.input.mouseWorldX();
-					aimY = Core.input.mouseWorldY();
-				}
+			let enemy = Units.closestTarget(
+				p.unit().team,
+				p.unit().x,
+				p.unit().y,
+				type.maxRange,
+				(u) => true,
+				(t) => true
+			);
+			if (enemy || Core.input.isTouched()) {
+				shouldShoot = true;
 			}
-
-			p.shooting = true;
-			p.unit().aim(aimX, aimY);
-			p.mouseX = aimX;
-			p.mouseY = aimY;
-
-			if (type.omniMovement && type.faceTarget) {
-				let mouseAngle = p.unit().angleTo(aimX, aimY);
-				if (lastUnitId !== p.unit().id) {
-					lastRotation = p.unit().rotation;
-					lastUnitId = p.unit().id;
-				}
-
-				let spd = p.unit().speedMultiplier;
-				if (typeof spd === 'function') spd = spd();
-				if (spd === undefined || spd === null) spd = 1;
-
-				lastRotation = Angles.moveToward(
-					lastRotation,
-					mouseAngle,
-					type.rotateSpeed * Time.delta * spd
-				);
-				p.unit().rotation = lastRotation;
-			}
-
-			p.unit().controlWeapons(true, true);
-		} else {
-			lastUnitId = -1;
 		}
-	});
-}
-
-module.exports = {
-	init: init,
-};
-
-init();
+	}
+	if (shouldShoot && !boosted) {
+		let aimX = 0;
+		let aimY = 0;
+		let type = p.unit().type;
+		if (!Vars.mobile) {
+			let mouse = Core.input.mouseWorld();
+			aimX = mouse.x;
+			aimY = mouse.y;
+		} else {
+			let enemy = Units.closestTarget(
+				p.unit().team,
+				p.unit().x,
+				p.unit().y,
+				type.maxRange,
+				(u) => true,
+				(t) => true
+			);
+			if (enemy) {
+				aimX = enemy.x;
+				aimY = enemy.y;
+			} else {
+				aimX = Core.input.mouseWorldX();
+				aimY = Core.input.mouseWorldY();
+			}
+		}
+		p.shooting = true;
+		p.unit().aim(aimX, aimY);
+		p.mouseX = aimX;
+		p.mouseY = aimY;
+		if (type.omniMovement && type.faceTarget) {
+			let mouseAngle = p.unit().angleTo(aimX, aimY);
+			if (lastUnitId !== p.unit().id) {
+				lastRotation = p.unit().rotation;
+				lastUnitId = p.unit().id;
+			}
+			let spd = p.unit().speedMultiplier;
+			if (typeof spd === 'function') spd = spd();
+			if (spd === undefined || spd === null) spd = 1;
+			lastRotation = Angles.moveToward(
+				lastRotation,
+				mouseAngle,
+				type.rotateSpeed * Time.delta * spd
+			);
+			p.unit().rotation = lastRotation;
+		}
+		p.unit().controlWeapons(true, true);
+	} else {
+		lastUnitId = -1;
+	}
+});
---
+++ scripts/features/plan_range.js
@@ -0,0 +1,86 @@
+const notify = require('qol-control/core/logger').notify;
+
+Events.run(Trigger.draw, () => {
+	if (!Vars.state.isGame() || !Vars.player || !Vars.player.unit()) return;
+
+	let input = Vars.control.input;
+	if (!input) return;
+
+	let plansMap = {};
+	let plans = [];
+
+	const addPlan = (p) => {
+		let key = p.x + ',' + p.y;
+		if (!plansMap[key]) {
+			plansMap[key] = p;
+			plans.push(p);
+		}
+	};
+
+	if (input.selectPlans && !input.selectPlans.isEmpty()) {
+		input.selectPlans.each(cons((p) => addPlan(p)));
+	}
+
+	if (input.linePlans && !input.linePlans.isEmpty()) {
+		input.linePlans.each(cons((p) => addPlan(p)));
+	}
+
+	if (Vars.player.unit().plans && Vars.player.unit().plans.size > 0) {
+		Vars.player.unit().plans.each(cons((p) => addPlan(p)));
+	}
+
+	if (plans.length === 0) return;
+
+	Draw.z(Layer.overlayUI);
+	Lines.stroke(1);
+
+	for (let i = 0; i < plans.length; i++) {
+		let plan = plans[i];
+		if (!plan.breaking && plan.block) {
+			let b = plan.block;
+
+			if (
+				b.category === Category.logic ||
+				b.category === Category.power ||
+				b.category === Category.distribution ||
+				b.category === Category.liquid
+			) {
+				continue;
+			}
+
+			let r = undefined;
+			if (typeof b.range === 'function') r = b.range();
+			else if (b.range !== undefined) r = b.range;
+			else if (b.radius !== undefined) r = b.radius;
+
+			if (r && r > 0) {
+				let px = plan.x * Vars.tilesize + b.offset;
+				let py = plan.y * Vars.tilesize + b.offset;
+
+				let color = Pal.accent;
+				if (b.name) {
+					if (b.name.indexOf('overdrive') > -1)
+						color = Color.valueOf('feb380');
+					else if (b.name.indexOf('force') > -1) color = Pal.shield;
+					else if (b.name.indexOf('mend') > -1) color = Pal.heal;
+					else if (b.category === Category.turret)
+						color = Color.valueOf('f25555');
+				}
+
+				Draw.color(color, 0.4);
+				if (b.sides !== undefined) {
+					Lines.poly(
+						px,
+						py,
+						b.sides,
+						r,
+						b.shieldRotation !== undefined ? b.shieldRotation : 0
+					);
+				} else {
+					Lines.circle(px, py, r);
+				}
+			}
+		}
+	}
+	Draw.reset();
+});
